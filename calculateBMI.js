76f2292fece21330d5fb992cbebec1fddcfb422b
vijay <vjkanduri@gmail.com>
2023-08-11 23:55:11 +0530

    UPDATE calculate BMI

--- calculateBMI.js
+++ calculateBMI.js
@@ -1,11 +1,11 @@
 function bmi(weight, height) {
   
-    bmi = weight / Math.pow(height, 2);
-    
-    if (bmi <= 18.5) { return "Underweight" }
-    if (bmi <= 25.0) { return "Normal" }
-    if (bmi <= 30.0) { return "Overweight" }
-    if (bmi > 30)    { return "Obese" }
+  BMI = weight / Math.pow(height, 2);
   
-  
-  }
+  if (BMI <= 18.5) { return "Underweight" }
+  if (BMI <= 25.0) { return "Normal" }
+  if (BMI <= 30.0) { return "Overweight" }
+  if (BMI > 30)    { return "Obese" }
+
+
+}
