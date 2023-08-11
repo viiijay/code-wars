c68080d14efac33abe5879c885c24fe075d6f9b6
vijay <vjkanduri@gmail.com>
2023-08-11 23:53:11 +0530

    SOLVED calculate BMI

---
+++ calculateBMI.js
@@ -0,0 +1,11 @@
+function bmi(weight, height) {
+  
+    bmi = weight / Math.pow(height, 2);
+    
+    if (bmi <= 18.5) { return "Underweight" }
+    if (bmi <= 25.0) { return "Normal" }
+    if (bmi <= 30.0) { return "Overweight" }
+    if (bmi > 30)    { return "Obese" }
+  
+  
+  }
